23b1a1dda7a74cc38a523e6f86d358581f454d7f
Robert Sherer <rsherer@gmail.com>
2020-11-30 23:02:43 -0800

    day 1 advent of code

--- day01/day01.py
+++ day01/day01.py
@@ -1,1 +1,38 @@
 import numpy as np
+from typing import List
+
+with open('input.txt') as f:
+    inputs = [int(num) for num in f.readlines()]
+
+example = [
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456
+]
+
+# part 1
+def find_2020_product(receipts: List[int]) -> int:
+    for exp1 in receipts:
+        for exp2 in receipts[1:]:
+            if exp1 + exp2 == 2020:
+                return exp1 * exp2
+
+assert find_2020_product(example) == 514579
+
+print(find_2020_product(inputs))
+
+# part 2
+
+def find_2020_product3(receipts: List[int]) -> int:
+    for exp1 in receipts:
+        for exp2 in receipts[1:]:
+            for exp3 in receipts[2:]:
+                if exp1 + exp2 + exp3 == 2020:
+                    return exp1 * exp2 * exp3
+
+assert find_2020_product3(example) == 241861950
+
+print(find_2020_product3(inputs))
