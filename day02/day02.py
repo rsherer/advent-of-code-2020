3ea584e0c3530e47ff4c91cc2866150cd29459b4
Robert Sherer <rsherer@gmail.com>
2020-12-02 10:09:41 -0800

    remove has_letter

--- day02/day02.py
+++ day02/day02.py
@@ -3,19 +3,13 @@
 from typing import List
 
 with open('day02.txt') as f:
-    inputs = [line.strip().split() for line in f]
+    passwords = [line.strip().split() for line in f]
 
 example = [
     ['1-3', 'a:', 'abcde'],
     ['1-3', 'b:', 'cdefg'],
     ['2-9', 'c:', 'ccccccccc'],
 ]
-
-def has_letter(letter: str, pwd: str) -> bool:
-    return letter in pwd
-
-assert has_letter('a', 'abcde') == True
-assert has_letter('b', 'cdefg') == False
 
 def in_range(lo: str, hi: str, letter: str, pwd: str) -> bool:
     counts = pwd.count(letter)
@@ -30,13 +24,13 @@ def count_valid_passwords(pwds: List[List[str]]) -> int:
             lo, hi = line[0].split('-')
             letter = line[1][0]
             password = line[2]
-            if sum([has_letter(letter, password), in_range(lo, hi, letter, password)]) == 2:
+            if in_range(lo, hi, letter, password):
                 total += 1
     return total
 
 assert count_valid_passwords(example) == 2, count_valid_passwords(example)
 
-print(count_valid_passwords(inputs))
+print(count_valid_passwords(passwords))
 
 # part 2
 
@@ -52,4 +46,4 @@ def count_valid_passwords2(pwds: List[List['str']]) -> int:
 
 assert count_valid_passwords2(example) == 1
 
-print(count_valid_passwords2(inputs))
+print(count_valid_passwords2(passwords))
