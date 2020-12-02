7f581bc8cae629fc5eecf0877b0d9bfca12276b3
Robert Sherer <rsherer@gmail.com>
2020-12-02 10:16:54 -0800

    black update

--- day01/day01.py
+++ day01/day01.py
@@ -1,16 +1,9 @@
 from typing import List
 
-with open('day01.txt') as f:
+with open("day01.txt") as f:
     inputs = [int(num) for num in f.readlines()]
 
-example = [
-    1721,
-    979,
-    366,
-    299,
-    675,
-    1456
-]
+example = [1721, 979, 366, 299, 675, 1456]
 
 # part 1
 def find_2020_product(receipts: List[int], year: int) -> int:
@@ -18,6 +11,7 @@ def find_2020_product(receipts: List[int], year: int) -> int:
         for exp2 in receipts[1:]:
             if exp1 + exp2 == year:
                 return exp1 * exp2
+
 
 assert find_2020_product(example, 2020) == 514579
 
@@ -30,6 +24,7 @@ def find_2020_product3(receipts: List[int], year: int) -> int:
             for exp3 in receipts[2:]:
                 if exp1 + exp2 + exp3 == year:
                     return exp1 * exp2 * exp3
+
 
 assert find_2020_product3(example, 2020) == 241861950
 
