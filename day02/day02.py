484d8633b1f9282932c71e3b078bae29cd045f26
Robert Sherer <rsherer@gmail.com>
2020-12-01 23:58:59 -0800

    refactor

--- day02/day02.py
+++ day02/day02.py
@@ -3,58 +3,54 @@
 from collections import Counter
 from typing import List
 
-def has_letter(policy: str, pwd: str) -> bool:
-    letter = policy[-1]
+with open('day02.txt') as f:
+    inputs = [line.strip().split() for line in f]
+
+example = [
+    ['1-3', 'a:', 'abcde'],
+    ['1-3', 'b:', 'cdefg'],
+    ['2-9', 'c:', 'ccccccccc'],
+]
+
+def has_letter(letter: str, pwd: str) -> bool:
     return letter in pwd
 
-assert has_letter('1-3 a', ': abcde') == True
-assert has_letter('1-3 b', ': cdefg') == False
+assert has_letter('a', 'abcde') == True
+assert has_letter('b', 'cdefg') == False
 
-def in_range(policy: str, pwd: str) -> bool:
-    lo, hi = policy.split('-')
-    lo, hi, letter = int(lo), int(hi[:-2]), hi[-1]
+def in_range(lo: str, hi: str, letter: str, pwd: str) -> bool:
     counts = Counter(pwd)
-    return counts[letter] in set(num for num in range(lo, hi + 1))
+    return int(lo) <= counts[letter] <= int(hi)
 
-assert in_range('1-3 a', ': abcde') == True
-assert in_range('1-3 b', ': cdefg') == False
+assert in_range('1', '3', 'a', 'abcde') == True
+assert in_range('1', '3', 'b', 'cdefg') == False
 
 def count_valid_passwords(pwds: List[str]) -> int:
     total = 0
     for line in pwds:
-        policy, pwd = line.split(': ')
-        if (has_letter(policy, pwd) and in_range(policy, pwd)):
-            total += 1
+            lo, hi = line[0].split('-')
+            letter = line[1][0]
+            password = line[2]
+            if sum([has_letter(letter, password), in_range(lo, hi, letter, password)]) == 2:
+                total += 1
     return total
 
-example = [
-    '1-3 a: abcde',
-    '1-3 b: cdefg',
-    '2-9 c: ccccccccc',
-]
-
-assert count_valid_passwords(example) == 2
-
-with open('day02.txt') as f:
-    inputs = [line.strip() for line in f]
+assert count_valid_passwords(example) == 2, count_valid_passwords(example)
 
 print(count_valid_passwords(inputs))
 
 # part 2
-# 
-#     
 
 def count_valid_passwords2(pwds: List['str']) -> int:
     total = 0
     for line in pwds:
-        policy, pwd = line.split(': ')
-        letter = policy[-1]
-        idx1, idx2 = policy[:-2].split('-')
-        idx1, idx2 = int(idx1), int(idx2)
-        if sum([pwd[idx1 - 1] == letter, pwd[idx2 - 1] == letter]) == 1:
+        lo, hi = line[0].split('-')
+        letter = line[1][0]
+        password = line[2]
+        if sum([password[int(lo) - 1] == letter, password[int(hi) - 1] == letter]) == 1:
             total += 1
     return total
 
-assert count_valid_passwords2(example) == 1, count_valid_passwords2(example)
+assert count_valid_passwords2(example) == 1
 
 print(count_valid_passwords2(inputs))
