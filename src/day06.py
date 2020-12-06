cd9f71248326862e430b2feaf7056b3819944357
Robert Sherer <rsherer@gmail.com>
2020-12-06 00:41:07 -0800

    edit to day 6 solution

--- src/day06.py
+++ src/day06.py
@@ -30,9 +30,9 @@ def count_all_answered(answers: List[str]) -> int:
             return len(same)
     return len(same)
 
-assert count_all_answered(['abc']) == 3, count_all_answered(['abc'])
+assert count_all_answered(['abc']) == 3
 assert count_all_answered(['a', 'b', 'c']) == 0
-assert count_all_answered(['ab', 'ac']) == 1, count_all_answered(['ab', 'ac'])
+assert count_all_answered(['ab', 'ac']) == 1
 
 print(sum([count_all_answered(answers) for answers in parse_questionnaires(qs)]))
 
